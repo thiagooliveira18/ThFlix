abe6a00fb1b79afb6b7cd0900a9441180be93ea8
Thiago Oliveira <62665295+thiagooliveira18@users.noreply.github.com>
2021-10-06 16:52:38 -0300

    Implementacao do Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
 import React from "react";
+
+import NetflixLogo from '../assets/netflix-logo.png';
+import Perfil from '../assets/perfil.png';
+
 import './Header.css'
 
-export default () => {
+export default ({ black }) => {
     return (
-        <header>
-            ...
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src={NetflixLogo} alt="Netflix" />
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src={Perfil} alt="Usuário" />
+                </a>
+            </div>
         </header>
     );
 }
